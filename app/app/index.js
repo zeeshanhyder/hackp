a3fea43bf1a4b68c9d99b492d91d61defdd3aaec
Zeeshan Hyder <hyder.zeeshan@live.com>
2018-03-31 10:55:14 -0400

    rollback

--- app/app/index.js
+++ app/app/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
-import {reactReduxFirebase} from 'react-redux-firebase';
+import {reactReduxFirebase, firebaseReducer} from 'react-redux-firebase';
 import {Provider, createStore} from 'react-redux';
-import {compose} from 'redux';
+import {compose, combineReducers} from 'redux';
 import {appState, rrfConfig, firebase_config} from './Reducers/reducers';
 import Navigator from "./config/routes";
 import firebase from 'firebase';
 
-
 firebase.initializeApp(firebase_config);
 
-const createStoreWithFirebase = compose(
-  reactReduxFirebase(firebase, rrfConfig),
-)(createStore)
+// const createStoreWithFirebase = compose(
+//   reactReduxFirebase(firebase, rrfConfig),
+// )(createStore)
 
-const initialState = {}
-const store = createStoreWithFirebase(appState, initialState)
+// const rootReducer = combineReducers({
+//   firebaseReducer
+// });
+
+// const initialState = {}
+// const store = createStoreWithFirebase(rootReducer, initialState, function(){})
 
 EStyleSheet.build({
   $primaryBlue: "#4f6d7a",
@@ -31,7 +34,5 @@ EStyleSheet.build({
 });
 
 export default () => (
-  <Provider store={store}>
     <Navigator />
-  </Provider>
 );
